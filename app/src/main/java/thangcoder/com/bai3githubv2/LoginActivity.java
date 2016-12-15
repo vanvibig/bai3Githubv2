315e8e562bb4cb59e4f4ce2f3035146f0569b28c
vanvibig <vanvibig@gmail.com>
2016-12-15 18:18:04 +0700

    commit change

---
+++ app/src/main/java/thangcoder/com/bai3githubv2/LoginActivity.java
@@ -0,0 +1,10 @@
+package thangcoder.com.bai3githubv2;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by KV on 15/12/2016.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
